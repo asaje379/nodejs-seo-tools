3397899451a257483ecfa2fd98f6abd675e30a72
asaje379 <salemaffa@gmail.com>
2023-06-06 22:56:17 +0100

    chore: Api title update

--- server/apps/core/src/main.ts
+++ server/apps/core/src/main.ts
@@ -7,12 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(CoreModule);
   app.enableCors();
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('SEO Tools Rest API')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 
   const msApp = await NestFactory.createMicroservice<MicroserviceOptions>(
